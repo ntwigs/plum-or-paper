58a5239bd24f69b1fe606692e4376e3f3a720722
ntwigs <oscar.cv.nordquist@gmail.com>
2023-04-16 19:44:23 +0200

    feat: add uppercase prop to text

--- src/components/text/index.tsx
+++ src/components/text/index.tsx
@@ -2,16 +2,27 @@ import { type TextProps, Text as BaseText, StyleSheet } from 'react-native'
 import { theme } from '../../theme'
 
 type Props = TextProps & {
-  color: keyof typeof theme.color
+  color?: keyof typeof theme.color
+  isUppercase?: boolean
 }
-export const Text = ({ children, color = 'dark', ...props }: Props) => {
+export const Text = ({
+  children,
+  color = 'dark',
+  isUppercase = false,
+  style,
+  ...props
+}: Props) => {
   return (
     <BaseText
       {...props}
-      style={{
-        ...styles.text,
-        color: theme.color[color],
-      }}
+      style={[
+        styles.text,
+        styles.uppercase,
+        {
+          color: theme.color[color],
+        },
+        style,
+      ]}
     >
       {children}
     </BaseText>
@@ -22,5 +33,9 @@ const styles = StyleSheet.create({
   text: {
     fontSize: 32,
     fontFamily: 'Nunito-Black',
+    textAlign: 'center',
+  },
+  uppercase: {
+    textTransform: 'uppercase',
   },
 })
