001fe81a1d6ee44fbde370f7c8445455bca7fa91
ntwigs <oscar.cv.nordquist@gmail.com>
2023-04-20 20:54:25 +0200

    feat: add score component

---
+++ src/components/score/index.tsx
@@ -0,0 +1,25 @@
+import { useEffect } from 'react'
+import { useSpring, animated } from '@react-spring/native'
+import { Text } from '../text'
+
+type Props = {
+  children: number
+}
+export const Score = ({ children }: Props) => {
+  const [spring, api] = useSpring(() => ({
+    from: { scale: 0.8 },
+    to: { scale: 1 },
+  }))
+
+  useEffect(() => {
+    api.start({reset: true})
+  }, [children])
+
+  return (
+    <animated.View style={{ transform: [{ scale: spring.scale }] }}>
+      <Text color="darker" size="larger">
+        {children}
+      </Text>
+    </animated.View>
+  )
+}
