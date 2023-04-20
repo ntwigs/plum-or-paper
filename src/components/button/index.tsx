c2e44975c9b1810c3c6f885aea79eaa30c284b29
ntwigs <oscar.cv.nordquist@gmail.com>
2023-04-20 20:56:00 +0200

    feat: handle button presses with animation

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -4,16 +4,41 @@ import {
   StyleSheet,
   ViewStyle,
   StyleProp,
+  View,
+  Text,
 } from 'react-native'
+import type { ReactNode } from 'react'
 import { theme } from '../../theme'
+import { useSpring, animated, to } from '@react-spring/native'
 
-type Props = PressableProps & {
+type Props = Omit<PressableProps, 'children'> & {
   style?: StyleProp<ViewStyle>
+  children: ReactNode
 }
 export const Button = ({ children, style = {}, ...props }: Props) => {
+  const [spring, api] = useSpring(() => ({
+    from: { y: 0 },
+  }))
+
   return (
-    <Pressable {...props} style={[styles.container, style]}>
-      {children}
+    <Pressable
+      {...props}
+      style={style}
+      onPressIn={(e) => {
+        api.start({ from: { y: 0 }, to: { y: 8 } })
+
+        if (props.onPressIn) {
+          props.onPressIn(e)
+        }
+      }}
+      onPressOut={() => api.start({ reverse: true })}
+    >
+      <View style={styles.shadow} />
+      <animated.View
+        style={[styles.container, { transform: [{ translateY: spring.y }] }]}
+      >
+        <View>{children}</View>
+      </animated.View>
     </Pressable>
   )
 }
@@ -27,5 +52,15 @@ const styles = StyleSheet.create({
     paddingHorizontal: 16,
     paddingVertical: 8,
     alignSelf: 'center',
+    width: '100%',
+  },
+  shadow: {
+    alignSelf: 'center',
+    width: '100%',
+    height: '100%',
+    position: 'absolute',
+    borderRadius: 16,
+    backgroundColor: theme.color.darker,
+    transform: [{ translateY: 8 }],
   },
 })
