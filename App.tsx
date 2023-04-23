65c84df9ddd5d8a745f2ee6094178ac96dd4d74b
ntwigs <oscar.cv.nordquist@gmail.com>
2023-04-23 20:46:02 +0200

    chore: remove react-navigation

--- App.tsx
+++ App.tsx
@@ -1,40 +1,12 @@
-import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, View } from 'react-native'
-import { useCallback } from 'react'
-import { useFonts } from 'expo-font'
-import * as SplashScreen from 'expo-splash-screen'
-import { theme } from './src/theme'
-import { Navigation } from './src/components/navigation'
 import { SoundProvider } from './src/hooks/use-sound'
+import { Main } from './src'
+import { StatusBar } from 'expo-status-bar'
 
 export default function App() {
-  const [fontsLoaded] = useFonts({
-    'Nunito-Black': require('./src/assets/fonts/Nunito-Black.ttf'),
-  })
-
-  const onLayoutRootView = useCallback(async () => {
-    if (fontsLoaded) {
-      await SplashScreen.hideAsync()
-    }
-  }, [fontsLoaded])
-
-  if (!fontsLoaded) {
-    return null
-  }
-
   return (
     <SoundProvider>
-      <View style={styles.container}>
-        <Navigation />
-        <StatusBar style="auto" />
-      </View>
+      <Main />
+      <StatusBar style="auto" />
     </SoundProvider>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: theme.color.light,
-  },
-})
