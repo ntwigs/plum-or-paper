bfd29b4949c8b7c3554d9b972099913b7c790e97
ntwigs <oscar.cv.nordquist@gmail.com>
2023-04-24 19:59:35 +0200

    refactor: move statusbar outside of context

--- App.tsx
+++ App.tsx
@@ -4,9 +4,11 @@ import { StatusBar } from 'expo-status-bar'
 
 export default function App() {
   return (
-    <SoundProvider>
-      <Main />
+    <>
+      <SoundProvider>
+        <Main />
+      </SoundProvider>
       <StatusBar style="auto" />
-    </SoundProvider>
+    </>
   )
 }
