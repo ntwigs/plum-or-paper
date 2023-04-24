f22c5a7970429587b7e34c75edcb3ad623df577c
ntwigs <oscar.cv.nordquist@gmail.com>
2023-04-24 20:00:04 +0200

    fix: make style optional for background

--- src/components/background/index.tsx
+++ src/components/background/index.tsx
@@ -3,7 +3,7 @@ import {animated, type AnimatedStyle } from '@react-spring/native'
 import { theme } from '../../theme'
 
 type Props = Omit<ViewProps, 'style'> & {
-  style: AnimatedStyle<ViewProps> & ViewProps['style']
+  style?: AnimatedStyle<ViewProps> & ViewProps['style']
 }
 export const Background = ({ children, style, ...props }: Props) => {
   return <animated.View style={[styles.container, style]} {...props}>{children}</animated.View>
