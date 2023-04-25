b5041ab1896d52693df95617d4597495fc2cfae2
ntwigs <oscar.cv.nordquist@gmail.com>
2023-04-25 20:54:34 +0200

    refactor: use game hooks

--- src/pages/game/index.tsx
+++ src/pages/game/index.tsx
@@ -1,133 +1,63 @@
 import { Background } from '../../components/background'
-import { Progress, TIMER_DURATION } from '../../components/progress'
+import { Progress } from '../../components/progress'
 import { View, StyleSheet } from 'react-native'
 import { Button } from '../../components/button'
 import { Text } from '../../components/text'
-import { ReactNode, useCallback, useEffect, useRef } from 'react'
-import { animated, useSpring, useSpringRef } from '@react-spring/native'
-import AnimatedLottieView from 'lottie-react-native'
 import { Phonetic } from '../../components/phonetic'
-import { useQuestion } from '../../hooks/use-question'
-import { useScore } from '../../hooks/use-score'
-import { useModal } from '../../hooks/use-modal'
+import { useQuestion } from './use-question'
 import { Confetti } from '../../components/confetti'
 import { GameOverModal } from '../../components/game-over-modal'
 import { Word } from '../../components/word'
 import { Score } from '../../components/score'
 import { StartModal } from '../../components/start-modal'
-import { useSound } from '../../hooks/use-sound'
-
-type HideProps = {
-  hideRef: any
-  children: ReactNode
-}
-const Hide = ({ hideRef, children }: HideProps) => {
-  const [contentSpring] = useSpring(() => ({
-    ref: hideRef,
-    from: { opacity: 0 },
-    to: { opacity: 1 },
-  }))
-
-  return <animated.View style={contentSpring}>{children}</animated.View>
-}
+import { useGameLogic } from './use-game-logic'
+import { useAnswer } from './use-answer'
+import { theme } from '../../theme'
 
 export const Game = () => {
-  const sound = useSound()
-  const confettiRef = useRef<AnimatedLottieView>(null)
-  const ref = useSpringRef<{ x: number }>()
-  const { isVisible: isStartModalVisible, closeModal: closeStartModal } =
-    useModal({ initialState: true })
-  const { answer, nextQuestion, question } = useQuestion()
-  const { increaseScore, resetScore, score } = useScore()
-  const { isVisible, closeModal, openModal } = useModal()
-  const hideRef = useSpringRef()
-
-  const startGame = useCallback(() => {
-    sound.score.replayAsync()
-    sound.button.replayAsync()
-    hideRef.start()
-    closeStartModal()
-    ref.current[0].start()
-    confettiRef.current.play()
-  }, [])
-
-  const selectAnswer = (choice: typeof answer) => () => {
-    if (answer === choice) {
-      ref.current[0].start({ reset: true })
-      sound.score.replayAsync()
-
-      confettiRef.current.play()
-
-      nextQuestion()
-      increaseScore()
-    } else {
-      gameOver()
-    }
-  }
-
-  const gameOver = () => {
-    hideRef.start({ opacity: 0 })
-    openModal()
-    ref.current[0].start({
-      config: {
-        duration: undefined,
-      },
-    })
-  }
-
-  const selectPlum = selectAnswer('plum')
-  const selectPaper = selectAnswer('paper')
-
-  const restart = () => {
-    resetScore()
-    nextQuestion()
-    closeModal()
-
-    hideRef.start({
-      opacity: 1,
-    })
-
-    ref.current[0].start({
-      reset: true,
-      config: {
-        duration: TIMER_DURATION,
-      },
-    })
-  }
+  const { nextQuestion, phonetic, spelling, language, answer } = useQuestion()
+  const {
+    restart,
+    startGame,
+    score,
+    onCorrect,
+    onWrong,
+    confettiRef,
+    timerRef,
+    isGameOverModalVisible,
+    isStartModalVisible,
+  } = useGameLogic({ nextQuestion })
+  const { selectPaper, selectPlum } = useAnswer({ answer, onCorrect, onWrong })
 
   return (
     <>
       <Background>
-        <Hide hideRef={hideRef}>
-          <View style={styles.container}>
-            <Score>{score}</Score>
-            <View>
-              <Text color="dark">{question.language}</Text>
-              <View style={{ paddingBottom: 12 }} />
-              <Word>{question[answer].spelling}</Word>
-              <View style={{ paddingTop: 12 }} />
-              <Phonetic>{question[answer].phonetic}</Phonetic>
-            </View>
-            <View>
-              <Button onPress={selectPlum}>
-                <Text color="darker">PLUM</Text>
-              </Button>
-              <View style={styles.space} />
-              <Button onPress={selectPaper}>
-                <Text color="darker">PAPER</Text>
-              </Button>
-            </View>
+        <View style={styles.container}>
+          <Score>{score}</Score>
+          <View>
+            <Text color="dark">{language}</Text>
+            <View style={styles.space} />
+            <Word>{spelling}</Word>
+            <View style={styles.space} />
+            <Phonetic>{phonetic}</Phonetic>
           </View>
-          <Progress ref={ref} onTimeout={gameOver} />
-        </Hide>
+          <View>
+            <Button onPress={selectPlum}>
+              <Text color="darker">PLUM</Text>
+            </Button>
+            <View style={styles.space} />
+            <Button onPress={selectPaper}>
+              <Text color="darker">PAPER</Text>
+            </Button>
+          </View>
+        </View>
+        <Progress ref={timerRef} onTimeout={onWrong} />
       </Background>
       <Confetti ref={confettiRef} />
       <GameOverModal
-        isVisible={isVisible}
-        answer={answer}
+        isVisible={isGameOverModalVisible}
         score={score}
         restart={restart}
-        word={question[answer].spelling}
       />
       <StartModal isVisible={isStartModalVisible} onPress={startGame} />
     </>
@@ -141,6 +71,6 @@ const styles = StyleSheet.create({
     justifyContent: 'space-around',
   },
   space: {
-    height: 16,
+    height: theme.getSize(2),
   },
 })
