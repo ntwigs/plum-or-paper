db2d23e401b43df5080c71e9fc6933a9c8908fbd
ntwigs <oscar.cv.nordquist@gmail.com>
2023-04-25 21:10:00 +0200

    fix: current score type name

--- src/components/current-score/index.tsx
+++ src/components/current-score/index.tsx
@@ -1,11 +1,11 @@
 import { memo } from 'react'
 import { Text } from '../text'
 
-type ScoreProps = {
+type Props = {
   score: number
   isVisible: boolean
 }
 export const CurrentScore = memo(
-  ({ score }: ScoreProps) => <Text align="left">{score}</Text>,
+  ({ score }: Props) => <Text align="left">{score}</Text>,
   (previous) => previous.isVisible
 )
