3d17c3a6bcb9a5ec4cbdb38bd6029c3f32e16717
ntwigs <oscar.cv.nordquist@gmail.com>
2023-04-25 20:55:24 +0200

    feat: add fade in animation

---
+++ src/components/overlay-fade-in/index.tsx
@@ -0,0 +1,25 @@
+import { StyleSheet, Dimensions } from 'react-native'
+import { animated, useSpring } from '@react-spring/native'
+import { theme } from '../../theme'
+
+const size = Dimensions.get('screen')
+
+export const OverlayFadeIn = () => {
+  const spring = useSpring({
+    from: { opacity: 1 },
+    to: { opacity: 0 },
+  })
+
+  return <animated.View pointerEvents="none" style={[styles.container, spring]} />
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: theme.color.light,
+    position: 'absolute',
+    width: size.width,
+    height: size.height,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+})
